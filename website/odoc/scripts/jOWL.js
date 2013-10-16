b4a6d8ad4bca468d01008d1a2db7bc52598cac57
Douglas R. Miles <logicmoo@gmail.com>
2013-10-16 21:14:41 +0000

    Beginning of Cross-Domain

--- website/odoc/scripts/jOWL.js
+++ website/odoc/scripts/jOWL.js
@@ -1231,6 +1231,61 @@ jOWL.data = function(rdfID, dtype, data){
 	d[rdfID][dtype] = data;
 };
 
+
+/**
+ * This is for Cross-site Origin Resource Sharing (CORS) requests.
+ *
+ * Additionally the script will fail-over to a proxy if you have one set up.
+ *
+ * @param string   url      the url to retrieve
+ * @param mixed    data     data to send along with the get request [optional]
+ * @param function callback function to call on successful result [optional]
+ * @param string   type     the type of data to be returned [optional]
+ */
+jOWL.getCORS = function(url, data, callback, type) {
+    try {
+        // Try using jQuery to get data
+        $.get(url, data, callback, type);
+    } catch(e) {
+        // jQuery get() failed, try IE8 CORS, or use the proxy
+        if (jQuery.browser.msie && window.XDomainRequest) {
+            // Use Microsoft XDR
+            var xdr = new XDomainRequest();
+            xdr.open("get", url);
+            xdr.onload = function() {
+                callback(this.responseText, 'success');
+            };
+            xdr.send();
+        } else {
+            try {
+                // Ancient browser, use our proxy
+                var mycallback = function() {
+                    var textstatus = 'error';
+                    var data = 'error';
+                    if ((this.readyState == 4)
+                        && (this.status == '200')) {
+                        textstatus = 'success';
+                        data = this.responseText;
+                    }
+                    callback(data, textstatus);
+                };
+                // proxy_xmlhttp is a separate script you'll have to set up
+                request = new proxy_xmlhttp();
+                request.open('GET', url, true);
+                request.onreadystatechange = mycallback;
+                request.send();
+            } catch(e) {
+                // Could not fetch using the proxy
+            }
+        }
+    }
+}
+
+jOWL.loadRemote = function(path, callback, options){
+	var that = this;
+	this.getCORS(path, function(xml){callback(that.parse(xml, options));});
+};
+
 /**
 * Initialize jOWL with an OWL-RDFS document.
 * @param path relative path to xml document
